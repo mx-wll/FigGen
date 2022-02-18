869309b5e11efcbaa781e1435c2eee7168ee3f00
Max Well <maximilian.well@gmail.com>
2022-02-18 23:02:31 +0100

    selection height/width added; Icons updated

--- public/code.js
+++ public/code.js
@@ -13,26 +13,54 @@ figma.showUI(__html__, { width: 232, height: 208 });
 figma.ui.onmessage = msg => {
     // One way of distinguishing between different types of messages sent from
     // your HTML page is to use an object with a "type" property like this.
+    let currentSelection = figma.currentPage.selection;
     if (msg.type === 'create-shapes') {
-        const nodes = [];
-        for (let i = 0; i < msg.count; i++) {
-            var shape;
-            if (msg.shape === 'rectangle') {
-                shape = figma.createRectangle();
+        console.log(msg);
+        let degrees = [0, 90, -180, -90];
+        // Create a Container
+        const containerFrame = figma.createFrame();
+        containerFrame.name = "Container";
+        containerFrame.x = currentSelection[0].x + currentSelection[0].width + 50;
+        containerFrame.y = currentSelection[0].y + currentSelection[0].width + 50;
+        containerFrame.resize(msg.count * msg.spacing, msg.count * msg.spacing);
+        containerFrame.paddingLeft = msg.spacing;
+        for (let j = 0; j < msg.count; j++) {
+            for (let i = 0; i < msg.count; i++) {
+                // Select Random Selection
+                let randomSelection = Array.from(Array(currentSelection.length).keys());
+                let randomContainer = Math.floor(Math.random() * randomSelection.length);
+                // Create Rectangle
+                const newRectangle = figma.currentPage.selection[randomContainer].clone();
+                // Mutate msg.spacing
+                let distanceY = (j * msg.spacing);
+                let distanceX = (i * msg.spacing);
+                newRectangle.x = distanceX;
+                newRectangle.y = distanceY;
+                {
+                    // Rotate – random 
+                    const random = Math.floor(Math.random() * degrees.length);
+                    // Rotate – Relative Transform
+                    let angle = degrees[random];
+                    let theta = angle * (Math.PI / 180);
+                    // Radians
+                    let cx = newRectangle.x + newRectangle.width / 2;
+                    let cy = newRectangle.y + newRectangle.height / 2;
+                    let newx = Math.cos(theta) * newRectangle.x + newRectangle.y * Math.sin(theta) - cy * Math.sin(theta) - cx * Math.cos(theta) + cx;
+                    let newy = -Math.sin(theta) * newRectangle.x + cx * Math.sin(theta) + newRectangle.y * Math.cos(theta) - cy * Math.cos(theta) + cy;
+                    newRectangle.relativeTransform = [[Math.cos(theta), Math.sin(theta), newx],
+                        [-Math.sin(theta), Math.cos(theta), newy]];
+                    // Add Rectangle to Frame
+                    containerFrame.appendChild(newRectangle);
+                }
             }
-            else if (msg.shape === 'triangle') {
-                shape = figma.createPolygon();
-            }
-            else {
-                shape = figma.createEllipse();
-            }
-            shape.x = i * 150;
-            shape.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(shape);
-            nodes.push(shape);
         }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
+        /* 		for (let i = 0; i < msg.count; i++) {
+        
+                    figma.currentPage.appendChild(shape);
+                    nodes.push(shape);
+                } */
+        //		figma.currentPage.selection = containerFrame;
+        //		figma.viewport.scrollAndZoomIntoView(containerFrame);
     }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
--- src/code.ts
+++ src/code.ts
@@ -6,7 +6,7 @@
 // full browser enviroment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, {width: 232, height: 208 });
+figma.showUI(__html__, { width: 232, height: 208 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
@@ -14,30 +14,75 @@ figma.showUI(__html__, {width: 232, height: 208 });
 figma.ui.onmessage = msg => {
 	// One way of distinguishing between different types of messages sent from
 	// your HTML page is to use an object with a "type" property like this.
+
+	let currentSelection = figma.currentPage.selection
+
 	if (msg.type === 'create-shapes') {
 
-		const nodes: SceneNode[] = [];
+		console.log(msg)
 
-		for (let i = 0; i < msg.count; i++) {
+		// Set degrees
+		let degreesSet = true
+		let degrees = [0, 90, -180, -90]
 
-			var shape;
+		// Create a Container
+		const containerFrame = figma.createFrame()
+		containerFrame.name = "Container"
+		containerFrame.x = currentSelection[0].x + currentSelection[0].width + 50;
+		containerFrame.y = currentSelection[0].y + currentSelection[0].width + 50;
+		containerFrame.resize(msg.count * msg.spacing, msg.count * msg.spacing);
+		containerFrame.paddingLeft = msg.spacing;
 
-			if (msg.shape === 'rectangle') {
-				shape = figma.createRectangle();
-			} else if (msg.shape === 'triangle') {
-				shape = figma.createPolygon();
-			} else {
-				shape = figma.createEllipse();
+		for (let j = 0; j < msg.count; j++) {
+
+			for (let i = 0; i < msg.count; i++) {
+
+				// Select Random Selection
+				let randomSelection = Array.from(Array(currentSelection.length).keys())
+				let randomContainer = Math.floor(Math.random() * randomSelection.length)
+
+				// Create Rectangle
+				const newRectangle = figma.currentPage.selection[randomContainer].clone()
+
+				// Mutate msg.spacing
+				let distanceY = (j * msg.spacing)
+				let distanceX = (i * msg.spacing)
+				newRectangle.x = distanceX
+				newRectangle.y = distanceY
+
+				if (degreesSet === true) {
+					// Rotate – random 
+					const random = Math.floor(Math.random() * degrees.length)
+
+					// Rotate – Relative Transform
+					let angle = degrees[random]
+					let theta = angle * (Math.PI / 180)
+
+					// Radians
+					let cx = newRectangle.x + newRectangle.width / 2
+					let cy = newRectangle.y + newRectangle.height / 2
+					let newx = Math.cos(theta) * newRectangle.x + newRectangle.y * Math.sin(theta) - cy * Math.sin(theta) - cx * Math.cos(theta) + cx
+					let newy = - Math.sin(theta) * newRectangle.x + cx * Math.sin(theta) + newRectangle.y * Math.cos(theta) - cy * Math.cos(theta) + cy
+					newRectangle.relativeTransform = [[Math.cos(theta), Math.sin(theta), newx],
+					[-Math.sin(theta), Math.cos(theta), newy]]
+
+					// Add Rectangle to Frame
+					containerFrame.appendChild(newRectangle)
+				} else {
+
+					// Add Rectangle to Frame
+					containerFrame.appendChild(newRectangle)
+				}
 			}
-
-			shape.x = i * 150;
-			shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-			figma.currentPage.appendChild(shape);
-			nodes.push(shape);
 		}
+		/* 		for (let i = 0; i < msg.count; i++) {
+		
+					figma.currentPage.appendChild(shape);
+					nodes.push(shape);
+				} */
 
-		figma.currentPage.selection = nodes;
-		figma.viewport.scrollAndZoomIntoView(nodes);
+		//		figma.currentPage.selection = containerFrame;
+		//		figma.viewport.scrollAndZoomIntoView(containerFrame);
 	}
 
 	// Make sure to close the plugin when you're done. Otherwise the plugin will
